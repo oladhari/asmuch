97d56cc82767e16f4413e8c747bafc6c456b65cc
Farid <farid.hammouten@gmail.con>
2019-01-04 00:11:40 +0900

    mobile ui ok and typo corrected

--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -23,68 +23,75 @@ import ProductSection from "./Sections/ProductSection";
 import TeamSection from "./Sections/TeamSection";
 import WorkSection from "./Sections/WorkSection";
 
+// Section Mobile UI
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import theme from "assets/jss/material-kit-react/views/componentsSections/theme";
+
+
+const muiTheme = createMuiTheme(theme);
 const dashboardRoutes = [];
 
 class LandingPage extends React.Component {
   render() {
     const { classes, ...rest } = this.props;
     return (
-      <div>
-        <Header
-          color="transparent"
-          routes={dashboardRoutes}
-          brand="As much 何とか"
-          rightLinks={<HeaderLinks />}
-          fixed
-          changeColorOnScroll={{
-            height: 400,
-            color: "white"
-          }}
-          {...rest}
-        />
-        <Parallax filter image={require("assets/img/bg8.jpg")}>
-          <div className={classes.container}>
-            <GridContainer justify="center">
-              <GridItem xs={12} sm={12} md={12}>
-                <h3 className={classes.title}>
-                  Asmuch, commitment through community.
-                </h3>
-                <h4>
-                  A rare place to find rare people, who are willing to do hard
-                  thinking, to work together and to learn.
-                </h4>
-              </GridItem>
-              <GridItem
-                xs={8}
-                sm={8}
-                md={10}
-                container
-                direction="row"
-                justify="center"
-                alignItems="center"
-              >
-                <Button
-                  variant="contained"
-                  color="info"
-                  href="https://join.slack.com/t/asmuchnantoka/shared_invite/enQtNDk3OTMyMjY1MzkzLTA2YTc0Y2YzZWE5NGZlMmQyODJkMjEzY2ZjYzZiZmY2NTdmNDg2ODQzMDdlYWEyNDRiOGI4OWM1MjY2ZWI5YWU"
-                  target="_blank"
-                  size="lg"
+      <MuiThemeProvider theme={muiTheme}>
+        <div>
+          <Header
+            color="transparent"
+            routes={dashboardRoutes}
+            brand="As much 何とか"
+            rightLinks={<HeaderLinks />}
+            fixed
+            changeColorOnScroll={{
+              height: 400,
+              color: "white"
+            }}
+            {...rest}
+          />
+          <Parallax filter image={require("assets/img/bg8.jpg")}>
+            <div className={classes.container}>
+              <GridContainer justify="center">
+                <GridItem xs={12} sm={12} md={12}>
+                  <h1 className={classes.title}>
+                    Asmuch, commitment through community.
+                </h1>
+                  <h2 className={classes.subtitle}>
+                    A rare place to find rare people, who are willing to do hard
+                    thinking, to work together and to learn.
+                </h2>
+                </GridItem>
+                <GridItem
+                  xs={8}
+                  sm={8}
+                  container
+                  direction="row"
+                  justify="center"
+                  alignItems="center"
                 >
-                  Direct connect to Slack Community
+                  <Button
+                    variant="contained"
+                    color="info"
+                    href="https://join.slack.com/t/asmuchnantoka/shared_invite/enQtNDk3OTMyMjY1MzkzLTA2YTc0Y2YzZWE5NGZlMmQyODJkMjEzY2ZjYzZiZmY2NTdmNDg2ODQzMDdlYWEyNDRiOGI4OWM1MjY2ZWI5YWU"
+                    target="_blank"
+                    size="lg"
+                  >
+                    Direct connect to Slack Community
                 </Button>
-              </GridItem>
-            </GridContainer>
+                </GridItem>
+              </GridContainer>
+            </div>
+          </Parallax>
+          <div className={classNames(classes.main, classes.mainRaised)}>
+            <div className={classes.container}>
+              <ProductSection />
+              <TeamSection />
+              <WorkSection />
+            </div>
           </div>
-        </Parallax>
-        <div className={classNames(classes.main, classes.mainRaised)}>
-          <div className={classes.container}>
-            <ProductSection />
-            <TeamSection />
-            <WorkSection />
-          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
