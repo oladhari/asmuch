6cfd993cb1f082f3e1068f8464267fc6f4a523d9
Farid <farid.hammouten@gmail.con>
2019-04-27 13:57:10 +0900

    fct state less

--- src/views/LandingPage/Sections/HiddenConsole.jsx
+++ src/views/LandingPage/Sections/HiddenConsole.jsx
@@ -1,67 +1,56 @@
-import React, { Component } from "react";
+import React from "react";
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Slide from "@material-ui/core/Slide";
+import Button from "@material-ui/core/Button";
+import DialogActions from "@material-ui/core/DialogActions"
+
 
 function Transition(props) {
   return <Slide direction="down" {...props} />;
 }
 
-class HiddenConsole extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false
-    };
-  }
+function HiddenConsole() {
+  const [open, setOpen] = React.useState(false);
 
-  handleClose = () => {
-    this.setState({
-      open: false
-    });
-  };
-
-  render() {
-    let user_keys = [];
-    let cnslCode = "67,79,78,83,79,76,69,13";
-    let SS2 = [
-      "%cSecret #2",
-      "color: #fff; background: #245060; padding:10px ;font-size: 1.5em; line-height: 2.2em;"
-    ];
-    document.onkeydown = event => {
-      user_keys.push(event.keyCode);
-      if (user_keys.toString().indexOf(cnslCode) >= 0) {
-        window.console.log.apply(console, SS2);
-        return this.setState({
-          open: true
-        });
-      }
-    };
-    return (
-      <div>
-        <Dialog
-          open={this.state.open}
-          TransitionComponent={Transition}
-          keepMounted
-          onClose={this.handleClose}
-          aria-labelledby="alert-dialog-slide-title"
-          aria-describedby="alert-dialog-slide-description"
-        >
-          <DialogTitle id="alert-dialog-slide-title">
-            {"Use Google's location service?"}
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-slide-description">
-              Let Google help apps determine location. This means sending
-              anonymous location data to Google, even when no apps are running.
-            </DialogContentText>
-          </DialogContent>
-        </Dialog>
-      </div>
-    );
+  function handleClose() {
+    setOpen(false);
   }
+  function handleOpen() {
+    setOpen(true);
+  }
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleOpen}>
+        Open console
+      </Button>
+      <Dialog
+        open={open}
+        onclose={handleClose}
+        TransitionComponent={Transition}
+        keepMounted
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
 
 export default HiddenConsole;
