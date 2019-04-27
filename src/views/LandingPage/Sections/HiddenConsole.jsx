3249c3230cc025eeb4163199c9a0b89822568e95
Farid <farid.hammouten@gmail.con>
2019-04-27 13:58:29 +0900

    fct es7 state less mui

--- src/views/LandingPage/Sections/HiddenConsole.jsx
+++ src/views/LandingPage/Sections/HiddenConsole.jsx
@@ -1,51 +1,46 @@
-import React from "react";
-import Dialog from "@material-ui/core/Dialog";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import Slide from "@material-ui/core/Slide";
-import Button from "@material-ui/core/Button";
-import DialogActions from "@material-ui/core/DialogActions"
-
-
-function Transition(props) {
-  return <Slide direction="down" {...props} />;
-}
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 function HiddenConsole() {
   const [open, setOpen] = React.useState(false);
 
+  function handleClickOpen() {
+    setOpen(true);
+  }
+
   function handleClose() {
     setOpen(false);
   }
-  function handleOpen() {
-    setOpen(true);
-  }
+
   return (
     <div>
-      <Button variant="outlined" color="primary" onClick={handleOpen}>
-        Open console
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Open alert dialog
       </Button>
       <Dialog
         open={open}
-        onclose={handleClose}
-        TransitionComponent={Transition}
-        keepMounted
-        aria-labelledby="alert-dialog-slide-title"
-        aria-describedby="alert-dialog-slide-description"
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-slide-title">
-          {"Use Google's location service?"}
-        </DialogTitle>
+        <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-            Let Google help apps determine location. This means sending
-            anonymous location data to Google, even when no apps are running.
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous location data to
+            Google, even when no apps are running.
           </DialogContentText>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
-            close
+            Disagree
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Agree
           </Button>
         </DialogActions>
       </Dialog>
