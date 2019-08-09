f8212ac036298c14161c3ac0e30c41e77324d575
Farid <farid.hammouten@gmail.con>
2019-08-10 00:43:03 +0900

    back to vanilla

--- src/assets/js/keepalive.js
+++ src/assets/js/keepalive.js
@@ -1,4 +1,5 @@
-import { get } from "http";
+var http = require("http");
 setInterval(function() {
-  get("http://asmuch.herokuapp.com");
-}, 600000); // every 10 minutes (300000)
+  http.get("www.asmuch.xyz");
+}, 300000);
+// 300000 request all 5 min
