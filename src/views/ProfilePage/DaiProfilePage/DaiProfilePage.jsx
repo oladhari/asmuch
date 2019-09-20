31b9f7d21f2da38604660eafb84baca054df5c11
Farid <farid.hammouten@gmail.con>
2019-09-21 00:24:19 +0900

    faprofile video and bg ok

--- src/views/ProfilePage/DaiProfilePage/DaiProfilePage.jsx
+++ src/views/ProfilePage/DaiProfilePage/DaiProfilePage.jsx
@@ -20,49 +20,6 @@ import { container, title } from "../../../assets/jss/material-kit-react.jsx";
 
 import InstagramEmbed from "react-instagram-embed";
 
-const imagesStyle = {
-  imgFluid: {
-    maxWidth: "100%",
-    height: "auto"
-  },
-  imgRounded: {
-    borderRadius: "6px !important"
-  },
-  imgRoundedCircle: {
-    borderRadius: "50% !important"
-  },
-  imgRaised: {
-    boxShadow:
-      "0 5px 15px -8px rgba(0, 0, 0, 0.24), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
-  },
-  imgGallery: {
-    width: "100%",
-    marginBottom: "2.142rem"
-  },
-  imgCardTop: {
-    width: "100%",
-    borderTopLeftRadius: "calc(.25rem - 1px)",
-    borderTopRightRadius: "calc(.25rem - 1px)"
-  },
-  imgCardBottom: {
-    width: "100%",
-    borderBottomLeftRadius: "calc(.25rem - 1px)",
-    borderBottomRightRadius: "calc(.25rem - 1px)"
-  },
-  imgCard: {
-    width: "100%",
-    borderRadius: "calc(.25rem - 1px)"
-  },
-  imgCardOverlay: {
-    position: "absolute",
-    top: "0",
-    right: "0",
-    bottom: "0",
-    left: "0",
-    padding: "1.25rem"
-  }
-};
-
 const profilePageStyle = {
   container,
   profile: {
@@ -83,7 +40,20 @@ const profilePageStyle = {
   name: {
     marginTop: "-80px"
   },
-  ...imagesStyle,
+  imgFluid: {
+    maxWidth: "100%",
+    height: "auto"
+  },
+  imgRounded: {
+    borderRadius: "6px !important"
+  },
+  imgRoundedCircle: {
+    borderRadius: "50% !important"
+  },
+  imgRaised: {
+    boxShadow:
+      "0 5px 15px -8px rgba(0, 0, 0, 0.24), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
+  },
   main: {
     background: "#FFFFFF",
     position: "relative",
